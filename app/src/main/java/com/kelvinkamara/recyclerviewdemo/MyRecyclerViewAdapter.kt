908ab0b2df32e1e07b8f5c78cc8fea3f1706d1cd
kkamara <kamaracomputers@gmail.com>
2024-01-22 17:03:11 +0000

    Refactor with Higher Order Function

--- app/src/main/java/com/kelvinkamara/recyclerviewdemo/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/kelvinkamara/recyclerviewdemo/MyRecyclerViewAdapter.kt
@@ -7,7 +7,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MyRecyclerViewAdapter(val fruitsList: List<Fruit>) : RecyclerView.Adapter<MyViewHolder>() {
+class MyRecyclerViewAdapter(
+    private val fruitsList: List<Fruit>,
+    private val clickListener: (Fruit) -> Unit
+) : RecyclerView.Adapter<MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInFlater = LayoutInflater.from(parent.context)
         val listItem = layoutInFlater.inflate(
@@ -24,21 +27,17 @@ class MyRecyclerViewAdapter(val fruitsList: List<Fruit>) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val fruit = fruitsList[position]
-        holder.bind(fruit)
+        holder.bind(fruit, clickListener)
     }
 }
 
 class MyViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-    fun bind(fruit: Fruit) {
+    fun bind(fruit: Fruit, clickListener: (Fruit) -> Unit) {
         val myTextView = view.findViewById<TextView>(R.id.tvName)
         myTextView.text = fruit.name
 
         view.setOnClickListener {
-            Toast.makeText(
-                view.context,
-                "Selected Fruit is : ${fruit.name}",
-                Toast.LENGTH_LONG
-            ).show()
+            clickListener(fruit)
         }
     }
 }
